036e729b2e725c9b4127d95662f4cf40c4d8d0f9
FedericaC03 <fdrk03@hotmail.it>
2020-08-26 16:17:47 +0200

    .

--- js/script.js
+++ js/script.js
@@ -1,15 +1,15 @@
-// 1. STAMPO IN CONSOLE NUMERI DA 1 A 100
-var lista = document.getElementById('prova');
- for (var num = 1; num <= 100; num++) {
-  lista.innerHTML += '<li>' + num + '</li>';
-}
+// 1. STAMPO I NUMERI DA 1 A 100
 // 2. PER I MULTIPLI DI 3 STAMPO "FIZZ" AL POSTO DEL NUMERO
 // 3. PER I MULTIPLI DI 5 STAMPO "BUZZ" AL POSTO DEL NUMERO
 // 4. PER I NUMERI MULTIPLI DI 3 E DI 5 STAMPO "FIZZBUZZ"
-if (num % 3 === 0) {
-  lista.replace(Fizz);
-} else if (num % 5 === 0) {
-  lista.replace("buzz");
-} else if (num % 3 === 0 || num % 5 === 0 ) {
-  lista.replace("fizzbuzz");
+
+ for (var num = 1; num <= 100; num++) {
+    console.log(num);
+  if (num % 3 === 0 && num % 5 === 0 ) {
+    console.log("fizz");
+  } else if (num % 3 === 0) {
+    console.log("buzz");
+  } else if (num % 5 === 0) {
+    console.log("fizzbuzz");
+  }
 }
