0b09966203f146aa8b6cc5b6dbdced05b47af84a
FedericaC03 <fdrk03@hotmail.it>
2020-08-26 18:20:41 +0200

    Fine esercizio

--- js/script.js
+++ js/script.js
@@ -1,15 +1,17 @@
-// 1. STAMPO I NUMERI DA 1 A 100
-// 2. PER I MULTIPLI DI 3 STAMPO "FIZZ" AL POSTO DEL NUMERO
-// 3. PER I MULTIPLI DI 5 STAMPO "BUZZ" AL POSTO DEL NUMERO
-// 4. PER I NUMERI MULTIPLI DI 3 E DI 5 STAMPO "FIZZBUZZ"
+// // 1. STAMPO I NUMERI DA 1 A 100
+// // 2. PER I MULTIPLI DI 3 STAMPO "FIZZ" AL POSTO DEL NUMERO
+// // 3. PER I MULTIPLI DI 5 STAMPO "BUZZ" AL POSTO DEL NUMERO
+// // 4. PER I NUMERI MULTIPLI DI 3 E DI 5 STAMPO "FIZZBUZZ"
 
+var lista = document.getElementById('lista');
  for (var num = 1; num <= 100; num++) {
-    console.log(num);
+
   if (num % 3 === 0 && num % 5 === 0 ) {
-    console.log("fizz");
+    lista.innerHTML += '<li>' + 'FizzBuzz' + '</li>';
   } else if (num % 3 === 0) {
-    console.log("buzz");
+    lista.innerHTML += '<li>' + 'Fizz' + '</li>';
   } else if (num % 5 === 0) {
-    console.log("fizzbuzz");
-  }
-}
+    lista.innerHTML += '<li>' + 'Buzz' + '</li>';
+  } else {
+    lista.innerHTML += '<li>' + num + '</li>';
+} }
